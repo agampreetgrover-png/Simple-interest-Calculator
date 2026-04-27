ac424c062ac45abbdf8c402f2145f229a4e3c4f8
agampreetgrover-png <agampreetgrover@gmail.com>
2026-04-27 21:52:54 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-# Simple Interest Calculator
-
-echo "Enter Principal:"
+echo "Enter the principal:"
 read p
-
-echo "Enter Rate of Interest:"
+echo "Enter time period in years:"
+read t
+echo "Enter rate of interest per year:"
 read r
 
-echo "Enter Time:"
-read t
-
-si=$((p * r * t / 100))
-
-echo "Simple Interest is: $si"
+s=$(echo "scale=2; $p * $t * $r / 100" | bc)
+echo "The simple interest is:"
+echo $s
